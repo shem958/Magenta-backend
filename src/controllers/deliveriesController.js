e62484c84d38a4ccefb7a3541a9a824f1ea2e11c
shem958 <shemgikunda37@gmail.com>
2025-02-03 18:27:39 +0300

    Refactor deliveriesController: enhance delivery methods with status responses and JSON formatting

--- src/controllers/deliveriesController.js
+++ src/controllers/deliveriesController.js
@@ -1,4 +1,19 @@
-export const getDeliveries = (req, res) => res.send("Get all deliveries");
-export const addDelivery = (req, res) => res.send("Add new delivery");
-export const updateDelivery = (req, res) => res.send("Update delivery");
-export const deleteDelivery = (req, res) => res.send("Delete delivery");
+export const getDeliveries = (req, res) => {
+  // Logic to get all deliveries
+  res.status(200).json({ message: "Get all deliveries" });
+};
+
+export const addDelivery = (req, res) => {
+  // Logic to add a new delivery
+  res.status(201).json({ message: "New delivery added" });
+};
+
+export const updateDelivery = (req, res) => {
+  // Logic to update a delivery
+  res.status(200).json({ message: "Delivery updated" });
+};
+
+export const deleteDelivery = (req, res) => {
+  // Logic to delete a delivery
+  res.status(200).json({ message: "Delivery deleted" });
+};
