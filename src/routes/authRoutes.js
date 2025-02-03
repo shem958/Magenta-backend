b4aa8b383d1e5c5a2cd10d8bc899db63a7e62ff0
shem958 <shemgikunda37@gmail.com>
2025-02-03 18:38:42 +0300

    Refactor authRoutes: remove unused finance routes and streamline registration endpoint

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,6 @@
-// backend/src/routes/authRoutes.js
 import express from "express";
-router.get("/", getFinance);
-router.post("/", addFinance);
-router.put("/:id", updateFinance);
-router.delete("/:id", deleteFinance);
-
 import { register, login } from "../controllers/authController.js";
+
 const router = express.Router();
 
 router.post("/register", register);
