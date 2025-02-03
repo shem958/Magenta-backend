3097ddb7dfc6345044f1c2c079f8ddf6004584ac
shem958 <shemgikunda37@gmail.com>
2025-02-03 18:22:38 +0300

    Add finance routes to authRoutes for managing financial operations

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,10 @@
 // backend/src/routes/authRoutes.js
 import express from "express";
+router.get("/", getFinance);
+router.post("/", addFinance);
+router.put("/:id", updateFinance);
+router.delete("/:id", deleteFinance);
+
 import { register, login } from "../controllers/authController.js";
 const router = express.Router();
 
