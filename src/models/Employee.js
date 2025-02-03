f445cb5785aec4be99cd9faf43bc078a5d031122
shem958 <shemgikunda37@gmail.com>
2025-02-03 18:34:22 +0300

    Refactor Employee model: enhance schema with required fields, default status, and timestamps

--- src/models/Employee.js
+++ src/models/Employee.js
@@ -1,7 +1,26 @@
-const employeeSchema = new mongoose.Schema({
-  name: String,
-  position: String,
-  salary: Number,
-  status: String,
-});
+import mongoose from "mongoose";
+
+const employeeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    position: {
+      type: String,
+      required: true,
+    },
+    salary: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: "Active",
+    },
+  },
+  { timestamps: true }
+);
+
 export default mongoose.model("Employee", employeeSchema);
