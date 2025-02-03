0a3b4438dce79f5a2f14a1ca14cb09097222e742
shem958 <shemgikunda37@gmail.com>
2025-02-03 18:42:57 +0300

    Refactor routes: reorganize route imports and definitions for clarity and consistency

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,18 @@
 // backend/src/routes/index.js
 import express from "express";
-import inventoryRoutes from "./inventoryRoutes.js";
+import deliveriesRoutes from "./deliveriesRoutes.js";
 import employeesRoutes from "./employeesRoutes.js";
 import financeRoutes from "./financeRoutes.js";
-import deliveriesRoutes from "./deliveriesRoutes.js";
+import inventoryRoutes from "./inventoryRoutes.js";
+import authRoutes from "./authRoutes.js";
 
 const router = express.Router();
 
 // Define API routes
-router.use("/inventory", inventoryRoutes);
+router.use("/auth", authRoutes);
+router.use("/deliveries", deliveriesRoutes);
 router.use("/employees", employeesRoutes);
 router.use("/finance", financeRoutes);
-router.use("/deliveries", deliveriesRoutes);
+router.use("/inventory", inventoryRoutes);
 
 export default router;
