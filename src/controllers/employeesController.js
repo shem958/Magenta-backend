fbce15355a1597d4d5996abcdbd6921a30d72483
shem958 <shemgikunda37@gmail.com>
2025-02-03 18:28:42 +0300

    Refactor employeesController: enhance employee methods with status responses and JSON formatting

--- src/controllers/employeesController.js
+++ src/controllers/employeesController.js
@@ -1,4 +1,19 @@
-export const getEmployees = (req, res) => res.send("Get all employees");
-export const addEmployee = (req, res) => res.send("Add new employee");
-export const updateEmployee = (req, res) => res.send("Update employee");
-export const deleteEmployee = (req, res) => res.send("Delete employee");
+export const getEmployees = (req, res) => {
+  // Logic to get all employees
+  res.status(200).json({ message: "Get all employees" });
+};
+
+export const addEmployee = (req, res) => {
+  // Logic to add a new employee
+  res.status(201).json({ message: "New employee added" });
+};
+
+export const updateEmployee = (req, res) => {
+  // Logic to update an employee
+  res.status(200).json({ message: "Employee updated" });
+};
+
+export const deleteEmployee = (req, res) => {
+  // Logic to delete an employee
+  res.status(200).json({ message: "Employee deleted" });
+};
