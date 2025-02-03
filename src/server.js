e8161a20e9fd0f6d467c4b873d3ab3b6d7aaf0e5
shem958 <shemgikunda37@gmail.com>
2025-02-03 18:45:24 +0300

    Refactor server.js: integrate route handling by importing routes from index.js

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "../config/db.js";
+import routes from "./routes/index.js";
 
 // Load environment variables
 dotenv.config();
@@ -16,6 +17,9 @@ const app = express();
 app.use(express.json()); // Parse JSON bodies
 app.use(cors()); // Enable CORS
 
+// Routes
+app.use("/api", routes);
+
 // Default route
 app.get("/", (req, res) => {
   res.send("Magenta API is running...");
@@ -24,5 +28,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
