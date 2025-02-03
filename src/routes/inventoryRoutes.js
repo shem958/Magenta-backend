23cee86046d030e138edee6e0bafc1b8c25d8569
shem958 <shemgikunda37@gmail.com>
2025-02-03 18:44:00 +0300

    Refactor inventoryRoutes: replace placeholder routes with controller methods for improved functionality

--- src/routes/inventoryRoutes.js
+++ src/routes/inventoryRoutes.js
@@ -1,11 +1,17 @@
 // backend/src/routes/inventoryRoutes.js
 import express from "express";
+import {
+  getInventory,
+  addInventory,
+  updateInventory,
+  deleteInventory,
+} from "../controllers/inventoryController.js";
+
 const router = express.Router();
 
-// Placeholder routes for inventory
-router.get("/", (req, res) => res.send("Get all inventory items"));
-router.post("/", (req, res) => res.send("Add new inventory item"));
-router.put("/:id", (req, res) => res.send("Update inventory item"));
-router.delete("/:id", (req, res) => res.send("Delete inventory item"));
+router.get("/", getInventory);
+router.post("/", addInventory);
+router.put("/:id", updateInventory);
+router.delete("/:id", deleteInventory);
 
 export default router;
