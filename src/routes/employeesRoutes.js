b26321d84e93a3efffbf8e53d29b42b472e043d6
shem958 <shemgikunda37@gmail.com>
2025-02-03 18:40:53 +0300

    Refactor employeesRoutes: replace placeholder routes with controller methods for improved functionality

--- src/routes/employeesRoutes.js
+++ src/routes/employeesRoutes.js
@@ -1,11 +1,17 @@
 // backend/src/routes/employeesRoutes.js
 import express from "express";
+import {
+  getEmployees,
+  addEmployee,
+  updateEmployee,
+  deleteEmployee,
+} from "../controllers/employeesController.js";
+
 const router = express.Router();
 
-// Placeholder routes for employees
-router.get("/", (req, res) => res.send("Get all employees"));
-router.post("/", (req, res) => res.send("Add new employee"));
-router.put("/:id", (req, res) => res.send("Update employee"));
-router.delete("/:id", (req, res) => res.send("Delete employee"));
+router.get("/", getEmployees);
+router.post("/", addEmployee);
+router.put("/:id", updateEmployee);
+router.delete("/:id", deleteEmployee);
 
 export default router;
