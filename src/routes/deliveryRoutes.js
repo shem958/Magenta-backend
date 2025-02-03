1fa6b3857ad64a6bed13dd569bdd43e5a1f7adf3
shem958 <shemgikunda37@gmail.com>
2025-02-03 18:39:52 +0300

    Refactor deliveryRoutes: replace placeholder routes with controller methods for better functionality

--- src/routes/deliveryRoutes.js
+++ src/routes/deliveryRoutes.js
@@ -1,11 +1,17 @@
 // backend/src/routes/deliveriesRoutes.js
 import express from "express";
+import {
+  getDeliveries,
+  addDelivery,
+  updateDelivery,
+  deleteDelivery,
+} from "../controllers/deliveriesController.js";
+
 const router = express.Router();
 
-// Placeholder routes for deliveries
-router.get("/", (req, res) => res.send("Get all deliveries"));
-router.post("/", (req, res) => res.send("Add new delivery"));
-router.put("/:id", (req, res) => res.send("Update delivery"));
-router.delete("/:id", (req, res) => res.send("Delete delivery"));
+router.get("/", getDeliveries);
+router.post("/", addDelivery);
+router.put("/:id", updateDelivery);
+router.delete("/:id", deleteDelivery);
 
 export default router;
